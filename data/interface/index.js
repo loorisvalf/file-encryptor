d62a26459dba382a8bf3494b3b64155cb07dc02e
loorisvalf <68768255+loorisvalf@users.noreply.github.com>
2022-01-18 07:33:35 +0000

    Add files via upload

--- data/interface/index.js
+++ data/interface/index.js
@@ -9,15 +9,18 @@ var config = {
       return chrome.runtime.getManifest().homepage_url;
     }
   },
-  "error": function (e) {
-    window.alert(e);
-    config.loader.hide();
-  },
   "key": function (p) {
     var usages = ["encrypt", "decrypt"];
     var buffer = config.file.convert.to.arraybuffer.string(p);
     var algo = {"digest": {"name": "SHA-256"}, "import": {"name": "AES-CBC"}};
     return crypto.subtle.digest(algo.digest, buffer).then(data => crypto.subtle.importKey("raw", data, algo.import, false, usages));
+  },
+  "error": function (e) {
+    window.alert(e);
+    config.loader.hide();
+    /*  */
+    document.querySelector(".info").removeAttribute("state");
+    document.querySelector(".result").removeAttribute("state");
   },
   "encrypt": async function (ivector, key) {
     try {
@@ -38,31 +41,15 @@ var config = {
   "password": function (name) {
     var password_1 = document.getElementById(name + "-password-1").value;
     var password_2 = document.getElementById(name + "-password-2").value;
+    /*  */
     if (!password_1) window.alert("Please enter a password in the 1st field (min 7 characters).");
-    else if (password_1.length < 7) window.alert("Please choose a longer password for the 1st field!");
     else if (!password_2) window.alert("Please enter a password in the 2nd field (min 7 characters).");
-    else if (password_2.length < 7) window.alert("Please choose a longer password for the 2nd field!");
-    else if (password_1 !== password_2) window.alert("Passwords (fields 1 & 2) must be the same!");
+    else if (password_1 && password_1.length < 7) window.alert("Please choose a longer password for the 1st field!");
+    else if (password_2 && password_2.length < 7) window.alert("Please choose a longer password for the 2nd field!");
+    else if (password_1 && password_2 &&  password_1 !== password_2) window.alert("Passwords (fields 1 & 2) must be the same!");
     else {}
     /*  */
     return password_1 && password_2 && password_1 === password_2 && password_1.length >= 7 ? password_1 : null;
-  },
-  "loader": {
-    "hide": function () {
-      var loader = document.querySelector(".loader");
-      if (loader) loader.remove();
-    },
-    "show": function () {
-      var loader = document.createElement("div");
-      var result = document.querySelector(".result");
-      /*  */
-      loader.setAttribute("class", "loader");
-      loader.appendChild(document.createElement("div"));
-      loader.appendChild(document.createElement("div"));
-      loader.appendChild(document.createElement("div"));
-      loader.appendChild(document.createElement("div"));
-      result.appendChild(loader);
-    }
   },
   "file": {
     "encrypt": null,
@@ -81,6 +68,23 @@ var config = {
       }
     }
   },
+  "loader": {
+    "hide": function () {
+      var loader = document.querySelector(".loader");
+      if (loader) loader.remove();
+    },
+    "show": function () {
+      var loader = document.createElement("div");
+      var result = document.querySelector(".result");
+      /*  */
+      loader.setAttribute("class", "loader");
+      loader.appendChild(document.createElement("div"));
+      loader.appendChild(document.createElement("div"));
+      loader.appendChild(document.createElement("div"));
+      loader.appendChild(document.createElement("div"));
+      result.appendChild(loader);
+    }
+  },
   "download": function (href, filename) {
     var a = document.createElement('a');
     var result = document.querySelector(".result");
@@ -88,8 +92,9 @@ var config = {
     /*  */
     a.href = href;
     a.download = filename;
-    a.textContent = '↓ ' + filename;
+    a.textContent = filename;
     result.appendChild(a);
+    result.setAttribute("state", filename.indexOf("encrypted") !== -1 ? "encrypted" : "decrypted");
     a.click();
     /*  */
     window.setTimeout(function () {
@@ -97,6 +102,50 @@ var config = {
       URL.revokeObjectURL(href);
       delete config.blob;
     }, 300);
+  },
+  "resize": {
+    "timeout": null,
+    "method": function () {
+      if (config.port.name === "win") {
+        if (config.resize.timeout) window.clearTimeout(config.resize.timeout);
+        config.resize.timeout = window.setTimeout(async function () {
+          var current = await chrome.windows.getCurrent();
+          /*  */
+          config.storage.write("interface.size", {
+            "top": current.top,
+            "left": current.left,
+            "width": current.width,
+            "height": current.height
+          });
+        }, 1000);
+      }
+    }
+  },
+  "port": {
+    "name": '',
+    "connect": function () {
+      config.port.name = "webapp";
+      var context = document.documentElement.getAttribute("context");
+      /*  */
+      if (chrome.runtime) {
+        if (chrome.runtime.connect) {
+          if (context !== config.port.name) {
+            if (document.location.search === "?tab") config.port.name = "tab";
+            if (document.location.search === "?win") config.port.name = "win";
+            if (document.location.search === "?popup") config.port.name = "popup";
+            /*  */
+            if (config.port.name === "popup") {
+              document.documentElement.style.width = "600px";
+              document.documentElement.style.height = "500px";
+            }
+            /*  */
+            chrome.runtime.connect({"name": config.port.name});
+          }
+        }
+      }
+      /*  */
+      document.documentElement.setAttribute("context", config.port.name);
+    }
   },
   "storage": {
     "local": {},
@@ -123,31 +172,27 @@ var config = {
       }
     }
   },
-  "port": {
-    "name": '',
-    "connect": function () {
-      config.port.name = "webapp";
-      var context = document.documentElement.getAttribute("context");
-      /*  */
-      if (chrome.runtime) {
-        if (chrome.runtime.connect) {
-          if (context !== config.port.name) {
-            if (document.location.search === "?tab") config.port.name = "tab";
-            if (document.location.search === "?win") config.port.name = "win";
-            if (document.location.search === "?popup") config.port.name = "popup";
-            /*  */
-            if (config.port.name === "popup") {
-              document.body.style.width = "600px";
-              document.body.style.height = "500px";
-            }
-            /*  */
-            chrome.runtime.connect({"name": config.port.name});
-          }
-        }
-      }
-      /*  */
-      document.documentElement.setAttribute("context", config.port.name);
-    }
+  "load": function () {
+    var reload = document.getElementById("reload");
+    var support = document.getElementById("support");
+    var donation = document.getElementById("donation");
+    /*  */
+    reload.addEventListener("click", function () {
+      document.location.reload();
+    }, false);
+    /*  */
+    support.addEventListener("click", function () {
+      var url = config.addon.homepage();
+      chrome.tabs.create({"url": url, "active": true});
+    }, false);
+    /*  */
+    donation.addEventListener("click", function () {
+      var url = config.addon.homepage() + "?reason=support";
+      chrome.tabs.create({"url": url, "active": true});
+    }, false);
+    /*  */
+    config.storage.load(config.app.start);
+    window.removeEventListener("load", config.load, false);
   },
   "app": {
     "start": function () {
@@ -161,26 +206,40 @@ var config = {
       /*  */
       encrypt.input.addEventListener("change", function (e) {
         if (e.target.files.length !== 1) return window.alert("Please select a file to encrypt!");
+        /*  */
         var filereader = new FileReader();
+        var info = document.querySelector(".info");
+        var result = document.querySelector(".result");
+        /*  */
+        result.textContent = '';
+        result.removeAttribute("state");
         config.file.encrypt = e.target.files[0];
+        info.textContent = config.file.encrypt.name;
         filereader.readAsArrayBuffer(config.file.encrypt);
-        document.querySelector(".result").textContent = '';
-        document.querySelector(".info").textContent = '↑ ' + config.file.encrypt.name;
         filereader.onload = function (e) {config.file.convert.to.arraybuffer.encrypt = e.target.result};
+        info.setAttribute("state", config.file.encrypt.name.indexOf("encrypted") !== -1 ? "encrypted" : "decrypted");
       });
       /*  */
       decrypt.input.addEventListener("change", function (e) {
         if (e.target.files.length !== 1) return window.alert("Please select a file to decrypt!");
+        /*  */
         var filereader = new FileReader();
+        var info = document.querySelector(".info");
+        var result = document.querySelector(".result");
+        /*  */
+        result.textContent = '';
+        result.removeAttribute("state");
         config.file.decrypt = e.target.files[0];
+        info.textContent = config.file.decrypt.name;
         filereader.readAsArrayBuffer(config.file.decrypt);
-        document.querySelector(".result").textContent = '';
-        document.querySelector(".info").textContent = '↓ ' + config.file.decrypt.name;
         filereader.onload = function (e) {config.file.convert.to.arraybuffer.decrypt = e.target.result};
+        info.setAttribute("state", config.file.decrypt.name.indexOf("encrypted") !== -1 ? "encrypted" : "decrypted");
       });
       /*  */
       encrypt.button.addEventListener("click", function () {
-        if (!config.file.convert.to.arraybuffer.encrypt) return window.alert("Please select a file to encrypt!");
+        if (!config.file.convert.to.arraybuffer.encrypt) {
+          return window.alert("Please select a file to encrypt!");
+        }
         /*  */
         var password = config.password("encrypt");
         var ivector = crypto.getRandomValues(new Uint8Array(16));
@@ -199,9 +258,15 @@ var config = {
                       var filename = config.file.encrypt.name + ".encrypted";
                       /*  */
                       config.download(href, filename);
-                    } else config.error("Error (0), corrupted file!");
-                  } else config.error("Error (1), corrupted file!");
-                } else config.error("Error (2), corrupted file!");
+                    } else {
+                      config.error("Error (0), corrupted file!");
+                    }
+                  } else {
+                    config.error("Error (1), corrupted file!");
+                  }
+                } else {
+                  config.error("Error (2), corrupted file!");
+                }
               });
             });
           }, 1000);
@@ -209,7 +274,9 @@ var config = {
       });
       /*  */
       decrypt.button.addEventListener("click", function () {
-        if (!config.file.convert.to.arraybuffer.decrypt) return window.alert("Please select a file to decrypt!");
+        if (!config.file.convert.to.arraybuffer.decrypt) {
+          return window.alert("Please select a file to decrypt!");
+        }
         /*  */
         var password = config.password("decrypt");
         var ivector = crypto.getRandomValues(new Uint8Array(16));
@@ -228,9 +295,15 @@ var config = {
                       var filename = config.file.decrypt.name.replace(".encrypted", '');
                       /*  */
                       config.download(href, filename);
-                    } else config.error("Error (0), incorrect password, wrong encrypted, or corrupted file!");
-                  } else config.error("Error (1), incorrect password, wrong encrypted, or corrupted file!");
-                } else config.error("Error (2), incorrect password, wrong encrypted, or corrupted file!");
+                    } else {
+                      config.error("Error (0), incorrect password, wrong encrypted, or corrupted file!");
+                    }
+                  } else {
+                    config.error("Error (1), incorrect password, wrong encrypted, or corrupted file!");
+                  }
+                } else {
+                  config.error("Error (2), incorrect password, wrong encrypted, or corrupted file!");
+                }
               });
             }, 1000);
           });
@@ -240,30 +313,11 @@ var config = {
   }
 };
 
-var load = function () {
-  var reload = document.getElementById("reload");
-  var support = document.getElementById("support");
-  var donation = document.getElementById("donation");
-  /*  */
-  window.addEventListener("resize", function (e) {
-    config.storage.write("width", window.innerWidth || window.outerWidth);
-    config.storage.write("height", window.innerHeight || window.outerHeight);
-  }, false);
-  /*  */
-  support.addEventListener("click", function (e) {
-    var url = config.addon.homepage();
-    chrome.tabs.create({"url": url, "active": true});
-  }, false);
-  /*  */
-  donation.addEventListener("click", function (e) {
-    var url = config.addon.homepage() + "?reason=support";
-    chrome.tabs.create({"url": url, "active": true});
-  }, false);
-  /*  */
-  config.storage.load(config.app.start);
-  window.removeEventListener("load", load, false);
-  reload.addEventListener("click", function () {document.location.reload()}, false);
-};
+config.port.connect();
+
+window.addEventListener("dragover", function (e) {
+  e.preventDefault();
+});
 
 window.addEventListener("drop", function (e) {
   if (!e.target.id || e.target.id.indexOf("-input") === -1) {
@@ -271,7 +325,5 @@ window.addEventListener("drop", function (e) {
   }
 });
 
-config.port.connect();
-
-window.addEventListener("load", load, false);
-window.addEventListener("dragover", function (e) {e.preventDefault()});
+window.addEventListener("load", config.load, false);
+window.addEventListener("resize", config.resize.method, false);
